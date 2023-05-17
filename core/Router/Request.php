9670bc0befff59fc56b8b58f29da810c71d1bee2
leon <leon00albert@gmail.com>
2023-05-17 10:06:16 +0300

    refactor: update /router/request and /router/utils to psr-1

--- core/Router/Request.php
+++ core/Router/Request.php
@@ -9,63 +9,90 @@ class Request
 {
     public $route = "";
 
+    /**
+     * @return [type]
+     */
     public function body()
     {
-        $request_body = file_get_contents('php://input');
+        $request_body = file_get_contents("php://input");
         $data = json_decode($request_body, true); // true to convert to associative array
 
         return $data;
 
     }
 
+    /**
+     * @return array
+     */
     public function query(): array
     {
         $parsed = parse_url($_SERVER["REQUEST_URI"]);
-        if (isset($parsed['query'])) {
-            $query = $parsed['query'];
+        if (isset($parsed["query"])) {
+            $query = $parsed["query"];
             parse_str($query, $params);
             return $params;
         }
 
     }
 
+    /**
+     * @return string
+     */
     public function method(): string
     {
-        $method = $_SERVER['REQUEST_METHOD'];
+        $method = $_SERVER["REQUEST_METHOD"];
         return $method;
     }
 
 
+    /**
+     * @return string
+     */
     public function path(): string
     {
         $parsed = parse_url($_SERVER["REQUEST_URI"]);
-        return $parsed['path'];
+        return $parsed["path"];
     }
 
+    /**
+     * @return string
+     */
     public function hostname(): string
     {
-        $hostname = $_SERVER['HTTP_HOST'];
+        $hostname = $_SERVER["HTTP_HOST"];
         return $hostname;
     }
 
+    /**
+     * @return string
+     */
     public function ip(): string
     {
-        $ip = $_SERVER['SERVER_ADDR'];
+        $ip = $_SERVER["SERVER_ADDR"];
         return $ip;
     }
 
+    /**
+     * @return string
+     */
     public function protocol(): string
     {
-        $protocol = $_SERVER['SERVER_PROTOCOL'];
+        $protocol = $_SERVER["SERVER_PROTOCOL"];
         return $protocol;
     }
 
+    /**
+     * @return bool
+     */
     public function xhr(): bool
     {
         $header = getallheaders();
-        return isset($header['XMLHttpRequest']);;
+        return isset($header["XMLHttpRequest"]);;
     }
 
+    /**
+     * @return array
+     */
     public function params(): array
     {
         if (Utils::hasParams($this->route)) {
@@ -77,9 +104,12 @@ class Request
 
     }
 
+    /**
+     * @return bool
+     */
     public function secure(): bool
     {
-        $secure = $_SERVER['HTTPS'];
+        $secure = $_SERVER["HTTPS"];
         return $secure;
     }
 
--- core/Router/Utils.php
+++ core/Router/Utils.php
@@ -3,23 +3,38 @@ namespace Artemis\Core\Router;
 
 class Utils
 {
+   /**
+    * @param string $url
+    * 
+    * @return array
+    */
     static function splitUrl(string $url): array
     {
-        $segments = explode('/', $url);
+        $segments = explode("/", $url);
         $lastSegment = end($segments);
         $lastSegmentIndex = array_key_last($segments);
-        $path = implode('/', array_slice($segments, 0, $lastSegmentIndex));
+        $path = implode("/", array_slice($segments, 0, $lastSegmentIndex));
         return array($path, $lastSegment);
     }
 
+   /**
+    * @param array $route
+    * 
+    * @return bool
+    */
     static function hasParams(array $route): bool
     {
-        return str_contains($route['path'], ":");
+        return str_contains($route["path"], ":");
     }
 
+   /**
+    * @param string $param
+    * 
+    * @return string
+    */
     static function parseParam(string $param): string
     {
-        $parsed = explode(':', $param);
+        $parsed = explode(":", $param);
         return $parsed[1];
     }
 
