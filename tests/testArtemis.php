32d2030e56abf7712a4ef048ab97a51ef4320225
leon <leon00albert@gmail.com>
2023-05-22 15:10:01 +0300

    feat: add tests for response router and request

---
+++ tests/testArtemis.php
@@ -0,0 +1,78 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+
+final class testArtemis extends TestCase
+{
+
+    public function testAppGet(): void
+    {
+        $url = 'http://localhost:8000/get' ;
+        $options = array(
+        'method' => 'GET',
+        );
+        $context = stream_context_create(array('http' => $options));
+        $response = file_get_contents($url, false, $context);
+
+      
+        $this->assertSame("200", trim($response));
+
+    }
+
+    public function testAppPost(): void
+    {
+        $form_data = array("test" => "true");
+        $url = 'http://localhost:8000/post';
+        $options = array(
+        'method' => 'POST',
+        'header' => 'Content-Type: application/json',
+        'content' => json_encode($form_data)
+
+        );
+        $context = stream_context_create(array('http' => $options));
+        $response = file_get_contents($url, false, $context);
+
+      
+        $this->assertSame("true", trim($response));
+        
+    }
+    public function testAppPut(): void
+    {
+        $form_data = array("test" => "true");
+        $url = 'http://localhost:8000/put';
+        $options = array(
+        'method' => 'PUT',
+        'header' => 'Content-Type: application/x-www-form-urlencoded',
+        'content' => http_build_query($form_data)
+
+        );
+        $context = stream_context_create(array('http' => $options));
+        $response = file_get_contents($url, false, $context);
+
+      
+        $this->assertSame("updated", trim($response));
+        
+    }
+    public function testAppDelete(): void
+    {
+        $form_data = array("test" => "true");
+        $url = 'http://localhost:8000/delete';
+        $options = array(
+        'method' => 'DELETE',
+        'header' => 'Content-Type: application/x-www-form-urlencoded',
+        'content' => http_build_query($form_data)
+
+        );
+        $context = stream_context_create(array('http' => $options));
+        $response = file_get_contents($url, false, $context);
+
+      
+        $this->assertSame("delete", trim($response));
+        
+    }
+
+
+}
+
+?>
